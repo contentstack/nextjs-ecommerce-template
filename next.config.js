5282e54c6afe8c3976917e81fad487a579e17b18
Amitkanswal <amit.kanswal@contentstack.com>
2020-07-24 13:06:16 +0530

    removed config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
-    api_key: "bltbbf9dda285996efa",
-    delivery_token: "csdc08bd4d7296c30e74f83772",
-    environment: "production",
+    api_key: "Your api key",
+    delivery_token: "Your delivery token",
+    environment: "Your publishing environment",
   },
 };
