72d6a6bf5fa105d2b737775b9e3a44e50065d5f3
Amitkanswal <amit.kanswal@contentstack.com>
2020-07-28 10:58:33 +0530

    added region setting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
-    api_key: "Your api key",
+    api_key: "Your apikey",
     delivery_token: "Your delivery token",
     environment: "Your publishing environment",
+    region: 'Your region'
   },
 };
--- sdk-plugins/index.js
+++ sdk-plugins/index.js
@@ -1,16 +1,17 @@
 const Contentstack = require("contentstack");
+const Stack = Contentstack.Stack({
+  api_key: process.env.api_key,
+  delivery_token: process.env.delivery_token,
+  environment: process.env.environment,
+  region: process.env.region ? process.env.region : "us",
+});
 
 export default {
   getEntryWithRef(ctUid, ref, locale) {
     return new Promise((resolve, reject) => {
-      const Stack = Contentstack.Stack({
-        api_key: process.env.api_key,
-        delivery_token: process.env.delivery_token,
-        environment: process.env.environment,
-      });
       Stack.ContentType(ctUid)
         .Query()
-        .language(locale || 'en-us')
+        .language(locale)
         .includeReference(ref)
         .includeOwner()
         .toJSON()
@@ -27,11 +28,6 @@ export default {
   },
   getEntryWithoutRef(ctUid, locale) {
     return new Promise((resolve, reject) => {
-      const Stack = Contentstack.Stack({
-        api_key: process.env.api_key,
-        delivery_token: process.env.delivery_token,
-        environment: process.env.environment,
-      });
       Stack.ContentType(ctUid)
         .Query()
         .language(locale)
@@ -50,11 +46,6 @@ export default {
   },
   getEntrySpecific(ctUid, entryId, locale) {
     return new Promise((resolve, reject) => {
-      const Stack = Contentstack.Stack({
-        api_key: process.env.api_key,
-        delivery_token: process.env.delivery_token,
-        environment: process.env.environment,
-      });
       Stack.ContentType(ctUid)
         .Entry(entryId)
         .language(locale)
@@ -68,13 +59,9 @@ export default {
           }
         );
     });
-  },  getEntrySpecificWithRef(ctUid, entryId,ref, locale) {
+  },
+  getEntrySpecificWithRef(ctUid, entryId, ref, locale) {
     return new Promise((resolve, reject) => {
-      const Stack = Contentstack.Stack({
-        api_key: process.env.api_key,
-        delivery_token: process.env.delivery_token,
-        environment: process.env.environment,
-      });
       Stack.ContentType(ctUid)
         .Entry(entryId)
         .language(locale)
@@ -92,28 +79,23 @@ export default {
   },
   getEntryWithQuery(ctUid, type, locale) {
     return new Promise((resolve, reject) => {
-    const Stack = Contentstack.Stack({
-      api_key: process.env.api_key,
-      delivery_token: process.env.delivery_token,
-      environment: process.env.environment,
+      Stack.ContentType(ctUid)
+        .Query()
+        .language(locale)
+        .toJSON()
+        .limit(9)
+        .query({ categories: { $in_query: { url: `/category/${type}` } } })
+        .only(["title", "url", "featured_image", "product_link", "price"])
+        .includeCount()
+        .find()
+        .then(
+          (result) => {
+            resolve(result);
+          },
+          (error) => {
+            reject(error);
+          }
+        );
     });
-    Stack.ContentType(ctUid)
-      .Query()
-      .language(locale)
-      .toJSON()
-      .limit(9)
-      .query({ categories: { $in_query: { url: `/category/${type}` } } })
-      .only(["title", "url", "featured_image", "product_link","price"]) 
-      .includeCount()
-      .find()
-      .then(
-        (result) => {
-          resolve(result);
-        },
-        (error) => {
-          reject(error);
-        }
-      );
-    })
   },
 };
