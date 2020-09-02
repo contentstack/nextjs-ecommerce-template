f48dafc9771bd2eb25ea6926f95d82b8b56a84e5
Amitkanswal <amit.kanswal@contentstack.com>
2020-09-02 11:03:22 +0530

    adding minor fixes

--- next.config.js
+++ next.config.js
@@ -10,9 +10,9 @@ module.exports = {
   publicRuntimeConfig: {
     // Will be available on both server and client
     staticFolder: '/static',
-    api_key: "bltbbf9dda285996efa",
-    delivery_token: "csdc08bd4d7296c30e74f83772",
-    environment: "production",
+    api_key: "Your API key",
+    delivery_token: "Your Delivery token",
+    environment: "Your Publishing Environment",
     region: 'us'
   },
 };
