f96cd78d799a3a8ec3ed446a2ac5782349e7c420
Amitkanswal <amit.kanswal@contentstack.com>
2020-09-10 16:37:19 +0530

    removed dynamic conf

--- next.config.js
+++ next.config.js
@@ -1,18 +1,23 @@
-
+// console.log(process.env);
 module.exports = {
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    api_key: process.env.api_key,
-    delivery_token:process.env.delivery_token,
-    environment: process.env.environment,
-    region: process.env.region
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    staticFolder: '/static',
+  env: {
     api_key: "bltbbf9dda285996efa",
     delivery_token: "csdc08bd4d7296c30e74f83772",
     environment: "production",
-    region: 'us'
   },
+  // serverRuntimeConfig: {
+  //   // Will only be available on the server side
+  //   api_key: process.env.api_key,
+  //   delivery_token:process.env.delivery_token,
+  //   environment: process.env.environment,
+  //   region: process.env.region
+  // },
+  // publicRuntimeConfig: {
+  //   // Will be available on both server and client
+  //   staticFolder: '/static',
+  //   api_key: "bltbbf9dda285996efa",
+  //   delivery_token: "csdc08bd4d7296c30e74f83772",
+  //   environment: "production",
+  //   region: 'us'
+  // },
 };
